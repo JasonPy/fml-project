b332aaddba53073536a074e5cc82dfc1efdc5408
Marie Litzinger <marie.litzinger@gmail.com>
2021-03-11 16:04:16 +0100

    Implemented epsilon greedy strategy w/o decay and softmax

--- agent_code/cherry_bomb/callbacks.py
+++ agent_code/cherry_bomb/callbacks.py
@@ -7,6 +7,13 @@ import numpy as np
 
 ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
 
+# hyperparamter / training config
+EPSILON_STRATEGY = "GREEDY_DECAY" # GREEDY / GREEDY_DECAY_SOFTMAX
+EPSILON_START_VALUE = 0.7
+EPSILON_END_VALUE = 0
+MAX_GAME_STEPS = 400
+TAU = 5 # IAUU softmax policy
+
 
 def setup(self):
     """
@@ -26,10 +33,23 @@ def setup(self):
         self.logger.info("Setting up model from scratch.")
         weights = np.random.rand(len(ACTIONS))
         self.model = weights / weights.sum()
+        self.epsilon = get_epsilon()
     else:
         self.logger.info("Loading model from saved state.")
         with open("my-saved-model.pt", "rb") as file:
             self.model = pickle.load(file)
+
+
+def get_epsilon():
+
+    epsilon = []
+
+    if (EPSILON_STRATEGY == "GREEDY_DECAY" ) or (EPSILON_STRATEGY == "GREEDY_DECAY_SOFTMAX"):
+        epsilon = np.linspace(EPSILON_START_VALUE, EPSILON_END_VALUE, MAX_GAME_STEPS)
+    elif EPSILON_STRATEGY == "GREEDY":
+        epsilon = np.linspace(EPSILON_START_VALUE, EPSILON_START_VALUE, MAX_GAME_STEPS)
+
+    return epsilon
 
 
 def act(self, game_state: dict) -> str:
@@ -43,10 +63,34 @@ def act(self, game_state: dict) -> str:
     """
     # todo Exploration vs exploitation
     random_prob = .1
-    if self.train and random.random() < random_prob:
-        self.logger.debug("Choosing action purely at random.")
+    if self.train: #and random.random() < random_prob:
+      
+        # computation of Q-values
+        # assumptions for value approximation of Q:
+        # 1. weights \beta_a are stored in a matrix column-wise -> multiplication of state_features with that matrix
+        #    leads to an array
+        # 2. weights \beta_a are stored in the same order as defined in ACTIONS 
+        state_features = state_to_features(game_state)
+        Q_sa = np.argmax(np.matmul(state_features.T, np.asarray(self.action_models_weight)))
+        argmax_Q = ACTIONS[np.argmax(Q_sa)]
+
+        if EPSILON_STRATEGY == "GREEDY_DECAY_SOFTMAX":
+            # IAUU exploration - improved epsilon-greedy strategy: uses softmax instead of uniform dist
+            
+            # calculation of probabilities for actions
+            numerator = np.exp(Q_sa/TAU)
+            denominator = np.sum(numerator)
+            probabilities = numerator/denominator
+            return np.random.choice(argmax_Q, np.random.choice(ACTIONS,probabilities),p=[1-self.epsilon[game_state.step],self.epsilon[game_state.step]])
+
+        else:
+            # uses uniform probability to pick an action with probability epsilon
+            return np.random.choice(argmax_Q, np.random.choice(ACTIONS),p=[1-self.epsilon[game_state.step],self.epsilon[game_state.step]])
+
+
+        # self.logger.debug("Choosing action purely at random.")
         # 80%: walk in any direction. 10% wait. 10% bomb.
-        return np.random.choice(ACTIONS, p=[.2, .2, .2, .2, .1, .1])
+        # return np.random.choice(ACTIONS, p=[.2, .2, .2, .2, .1, .1])
 
     self.logger.debug("Querying model for action.")
     return np.random.choice(ACTIONS, p=self.model)
