6f298d8f193dc06da814d61b1f2e8d732ed5a36b
Mavke <marven.hinze@gmx.de>
2021-03-22 16:42:53 +0100

    added train data

--- agent_code/cherry_bomb/deep_q_net.py
+++ agent_code/cherry_bomb/deep_q_net.py
@@ -11,8 +11,8 @@ class DeepQNet(nn.Module):
     def __init__(self, num_states, seed):
         super(DeepQNet, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.input_layer = nn.Linear(num_states, 64)
-        self.output_layer = nn.Linear(64, ACTIONS)
+        self.input_layer = nn.Linear(num_states, 50)
+        self.output_layer = nn.Linear(50, ACTIONS)
 
     def forward(self, x):
         x = F.relu(self.input_layer(x))
